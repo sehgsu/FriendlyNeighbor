d8d787ef10db03a0865eab5a73cd4416a8e4f382
Ismael Rivera <i.rvra130@gmail.com>
2019-06-15 10:42:49 -0400

    updated condition to description in db

--- models/Inventory.js
+++ models/Inventory.js
@@ -1,9 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
  var Inventory = sequelize.define("Inventory", {
     product_name: DataTypes.STRING,
+    description: DataTypes.STRING,
     category: DataTypes.STRING,
     price: DataTypes.INTEGER,
-    conditions: DataTypes.STRING  
+      
 });
 return Inventory;
 };
